1df04cdc152480591ec94d0e832d35f011e06df6
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-23 01:11:32 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# empty list
+print(list())
+#-->[]
+
+# vowel string
+vowel_string = 'aeiou'
+print(list(vowel_string))
+#-->['a', 'e', 'i', 'o', 'u']
+
+# vowel tuple
+vowel_tuple = ('a', 'e', 'i', 'o', 'u')
+print(list(vowel_tuple))
+#-->['a', 'e', 'i', 'o', 'u']
+
+# vowel list
+vowel_list = ['a', 'e', 'i', 'o', 'u']
+print(list(vowel_list))
+#-->['a', 'e', 'i', 'o', 'u']
